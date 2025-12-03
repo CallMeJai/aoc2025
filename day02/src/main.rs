2897a83c7faa4616304aa0a0793c5a353a57556f
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2025-12-02 16:44:13 -0800

    day 02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let input = include_str!("../inputs/input.txt");
     let result = part_1(input);
     println!("Part 1: {result}");
+    let result = part_2(input);
+    println!("Part 2: {result}");
 }
 
 fn part_1(s: &str) -> u64 {
@@ -10,6 +12,19 @@ fn part_1(s: &str) -> u64 {
     for range in ranges.iter() {
         for num in range.0..=range.1 {
             if is_doubled_sequence(num) {
+                sum += num;
+            }
+        }
+    }
+    sum
+}
+
+fn part_2(s: &str) -> u64 {
+    let mut sum = 0;
+    let ranges = parse_to_ranges(s);
+    for range in ranges.iter() {
+        for num in range.0..=range.1 {
+            if is_repeated_sequence(num) {
                 sum += num;
             }
         }
@@ -35,6 +50,41 @@ fn is_doubled_sequence(num: u64) -> bool {
     return num / 10_u64.pow(num.ilog10() / 2 + 1) == num % 10_u64.pow(num.ilog10() / 2 + 1);
 }
 
+fn is_repeated_sequence(num: u64) -> bool {
+    if num < 10 {
+        return false;
+    }
+    'chunk_size: for sequence_length in 1..=(num.ilog10() as usize / 2 + 1) {
+        if let Some(chunks) = num_into_chunks(num, sequence_length) {
+            let start = chunks[0];
+            for chunk in chunks.into_iter() {
+                if chunk != start {
+                    continue 'chunk_size;
+                }
+            }
+            return true;
+        }
+    }
+    false
+}
+
+fn num_into_chunks(num: u64, len: usize) -> Option<Vec<u64>> {
+    let mut v = Vec::new();
+    let mut x = num;
+    if len > num.ilog10() as usize + 1 {
+        None
+    } else if (num.ilog10() as usize + 1) % len != 0 {
+        None
+    } else {
+        while x != 0 {
+            v.push(x % 10_u64.pow(len as u32));
+            x /= 10_u64.pow(len as u32);
+        }
+        v.reverse();
+        Some(v)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +93,12 @@ mod tests {
         let input = include_str!("../inputs/sample.txt");
         let result = part_1(input);
         assert_eq!(result, 1227775554);
+    }
+
+    #[test]
+    fn day_02_part_2_test() {
+        let input = include_str!("../inputs/sample.txt");
+        let result = part_2(input);
+        assert_eq!(result, 4174379265);
     }
 }
